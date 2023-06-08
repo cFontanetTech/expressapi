98c1c9ec9f914ec7273756be877357fec384250a
cFontanetTech <130543107+cFontanetTech@users.noreply.github.com>
2023-06-08 14:52:27 +0000

    crud

--- client.js
+++ client.js
@@ -13,8 +13,78 @@ export async function postPizza() {
     })
     const data = await response.json();
     const {status} = response;
+    const type = "POST"
     console.log({
+        type,
         status,
         data
+    })
+}
+
+export async function getPizzas(){
+    const response = await fetch(`${baseUrl}/pizzas`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+    })
+    const data = await response.json();
+    const {status} = response;
+    const type = "GET"
+    console.log({
+        type,
+        status,
+        data
+    })
+}
+
+export async function getPizza(id){
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+    })
+    const data = await response.json();
+    const {status} = response;
+    const type = "GET by ID"
+    console.log({
+        type,
+        status,
+        data
+    })
+}
+
+export async function putPizza(id){
+    const pizza = {
+        name: 'hawaiana'
+    }
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(pizza)
+    })
+    const {status} = await response;
+    const type = "PUT"
+    console.log({
+        type,
+        status
+    })
+}
+
+export async function deletePizza(id){
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    const {status} = await response;
+    const type = "DELETE"
+    console.log({
+        type,
+        status
     })
 }
--- serve.js
+++ serve.js
@@ -1,5 +1,15 @@
 import expres from "express";
-import {postPizza} from './client.js'
+import {postPizza} from './client.js';
+import {getPizzas} from './client.js';
+import {getPizza} from './client.js';
+import {putPizza} from './client.js';
+import {deletePizza} from './client.js';
+
+const pizzas = [{
+    name: 'carbonara',
+},{
+    name: 'quesos',
+}]
 
 const app = expres();
 
@@ -9,6 +19,34 @@ app.post('/pizzas',(req,res)=>{
     res.status(201).json(req.body)
 })
 
+app.get('/pizzas', (req,res) => {
+    res.status(200).json(pizzas)
+})
+
+app.get('/pizzas/:id', (req,res) => {
+    const {id} = req.params
+    res.status(200).json(pizzas[id])
+})
+
+app.put('/pizzas/:id',(req,res)=>{
+    const {id} = req.params
+    pizzas[id] = req.body
+    res.status(200).end('')
+})
+
+app.delete('/pizzas/:id',(req,res)=>{
+    const {id} = req.params
+    //pizzas.delete[id]
+    pizzas.splice(id, 1);
+    res.status(200).end('')
+})
+//get id & arrat +  put + delete
+
 const server = app.listen(8080);
 
-postPizza();
+await postPizza();
+await getPizzas();
+await putPizza(0);
+await getPizza(0);
+await deletePizza(1);
+await getPizzas();
